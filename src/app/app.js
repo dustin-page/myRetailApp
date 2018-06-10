5b9acf2283c29f5d827e98e46531781c6789c50c
Dustin Page <dustinpage@live.com>
2018-06-10 13:49:04 -0500

    Create carousel to scroll through images

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,15 @@
 import angular from 'angular';
-import jquery from 'jquery';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 import bootstrap from 'bootstrap';
+
+
+import slickCarousel from 'slick-carousel';
+import slick from 'angular-slick';
+
+import '../../node_modules/slick-carousel/slick/slick.scss';
+import '../../node_modules/slick-carousel/slick/slick-theme.scss';
 
 import uirouter from 'angular-ui-router';
 //Add routing
@@ -32,7 +41,7 @@ const MODULE_NAME = 'app';
 /*
 The convention that all external modules are following is to simply export the name of the module, so uirouter here is the string 'uirouter'.
 */
-angular.module(MODULE_NAME, [uirouter, productDetails])
+angular.module(MODULE_NAME, [uirouter, slick, productDetails])
   // .directive('app', app)
   // .controller('AppCtrl', AppCtrl)
   .config(routing);
